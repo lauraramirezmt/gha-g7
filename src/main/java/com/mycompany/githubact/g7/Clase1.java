a7ef55e26718ac9a36f6e6468443adb0e217c8eb
lauraramirezmt <laura.ramirez1.mt@correo.usa.edu.co>
2021-10-27 05:57:59 -0600

    primer commit con neatbeans en proyecto prueba

---
+++ src/main/java/com/mycompany/githubact/g7/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.githubact.g7;
+
+/**
+ *
+ * @author Laura
+ */
+public class Clase1 {
+    
+    public int sumar(int a, int b){
+        return a + b;
+    }
+    
+}
---
+++ src/test/java/com/mycompany/githubact/g7/Clase1Test.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.githubact.g7;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Laura
+ */
+public class Clase1Test {
+    
+    public Clase1Test() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+            
+    /**
+     * Test of sumar method, of class Clase1.
+     */
+    @org.junit.jupiter.api.Test
+    public void testSumar() {
+        System.out.println("sumar");
+        Clase1 instance = new Clase1();
+        int result = instance.sumar(5, 4);
+        assertEquals(9, result);
+        
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testSumar2() {
+        System.out.println("sumar");
+        Clase1 instance = new Clase1();
+        int result = instance.sumar(10, 50);
+        assertEquals(60, result);
+        
+    } 
+}
